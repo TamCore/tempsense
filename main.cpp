63469ce58cdc9e77518795c8eb8b452e48e32392
TamCore <philipp@tamcore.eu>
2015-06-08 20:55:30 +0200

    reorganize code, display sensor num, sensor count, sensor serial and power supply

--- main.cpp
+++ main.cpp
@@ -1,48 +1,42 @@
 #include <hidapi.h>
+#include <cstdlib>
 #include <cstdio>
 #include <ctime>
 
 int main(int argc, char **argv) {
-    
+    int i, num;
+    float temp;
+    unsigned char buf[64];
+    char *pwr;
+
     hid_device *handle = hid_open(0x16c0, 0x0480, nullptr);
     if (!handle) {
         fprintf(stderr, "No temperature sensor found!\n");
         return 1;
     }
     
-    const int MAX_STR = 255;
-    wchar_t wstr[MAX_STR];
-    
-    printf("Device Info:\n");
-    int res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-    if (res == -1) {
-        fprintf(stderr, "Could not get manufacturer string!\n");
-        return 2;
-    }
-    printf("\tManufacturer String: %ls\n", wstr);
-    
-    res = hid_get_product_string(handle, wstr, MAX_STR);
-    if (res == -1) {
-        fprintf(stderr, "Could not get product string!\n");
-        return 3;
-    }
-    printf("\tProduct String: %ls\n", wstr);
-    
-    unsigned char buf[65];
-    for (;;) {
-        int num = hid_read(handle, buf, 64);
+    while (true) {
+        num = hid_read(handle, buf, 64);
         if (num < 0) {
             fprintf(stderr, "Could not read from device!\n");
             return 4;
         }
         if (num == 64) {
+            if (buf[2]) { pwr = "Extern"; }
+            else { pwr = "Parasite"; }
+
+            temp = *(short *)&buf[4];
+
             time_t now;
             time(&now);
             tm loctm;
             localtime_r(&now, &loctm);
-            
-            short temp = *(short *)&buf[4]; //holy fuck!
-            printf("<%02d:%02d:%02d> temp: %+.1f°\n",loctm.tm_hour, loctm.tm_min, loctm.tm_sec, (float)temp/10.0f);
+
+            fprintf(stdout, "[%02d:%02d:%02d] Sensor #%d of %d: \t %.1f\u2103 \t Power: %-10s \t ID: ", loctm.tm_hour, loctm.tm_min, loctm.tm_sec, buf[1], buf[0], (float)temp/10.0f, pwr);
+            for (i = 0x08; i < 0x10; i++) {
+                fprintf(stdout, "%02X ", (unsigned char)buf[i]);
+            }
+            printf("\n");
         }
     }
     return 0;
